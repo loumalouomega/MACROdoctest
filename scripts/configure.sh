2e21584f6469e1e64fd6f272c06f4f10bbcea609
Vicente Mataix Ferrándiz <tote1989@gmail.com>
2021-09-20 16:13:29 +0200

    Script

---
+++ scripts/configure.sh
@@ -0,0 +1,12 @@
+# Ensuring that everything is up-to-date
+git submodule sync --recursive
+git submodule update --init --recursive
+git submodule foreach git pull origin master
+
+# Set variables
+export FOLDER_SOURCE="${FOLDER_SOURCE:-"$( cd "$(dirname "$0")" ; pwd -P )"/..}"
+export FOLDER_BUILD="${FOLDER_SOURCE}/build"
+
+# Configure
+cmake --no-warn-unused-cli ..
+cmake --build "${FOLDER_BUILD}" --target install -- -j$(nproc)
